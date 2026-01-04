2b6451a60f6c63cb583449c608409cfcedd449ad
NoahDD-01 <noahdd216@gmail.com>
2026-01-04 23:27:48 +0630

    update

--- src/constants/endpoint.jsx
+++ src/constants/endpoint.jsx
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://127.0.0.1:8000";
+// export const BASE_URL = "http://127.0.0.1:8000";
 
-// export const BASE_URL = "https://noahdd.pythonanywhere.com"
+export const BASE_URL = "https://noahdd.pythonanywhere.com"
 
 export const ENDPOINTS = {
     // Contact Us Endpoints
